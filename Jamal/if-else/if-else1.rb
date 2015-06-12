cea08bee4ef10690db519f9235d2633256b4908f
Jamalludin <jamal.schneider@gmail.com>
2015-06-12 22:58:03 +0700

    menghapus beberapa baris

---
+++ Jamal/if-else/if-else1.rb
@@ -0,0 +1,7 @@
+puts "masukan angka : "
+a=gets.to_i
+if a % 2 !=0
+	puts "maka ganjil"
+else
+	puts "maka bukan ganjil"
+end	
--- Jamal/variables/variables1.rb
+++ Jamal/variables/variables1.rb
@@ -18,6 +18,7 @@ class2obj = Class2.new
 class2obj.print_global
 
 class Customer
+	@@no_of_customers=0
    def initialize(id, name, addr)
       @cust_id=id
       @cust_name=name
@@ -27,6 +28,10 @@ class Customer
       puts "Customer id #@cust_id"
       puts "Customer name #@cust_name"
       puts "Customer address #@cust_addr"
+    end
+def total_no_of_customers()
+       @@no_of_customers += 1
+       puts "Total Customer : #@@no_of_customers"
     end
 end
 
@@ -38,3 +43,11 @@ cust3=Customer.new("3", "fikri", "kos pak subari, tegal catak")
 cust1.display_details()
 cust2.display_details()
 cust3.display_details()
+
+puts "masukan angka : "
+a=gets.to_i
+if a % 2 !=0
+	puts "maka ganjil"
+else
+	puts "maka bukan ganjil"
+end	
