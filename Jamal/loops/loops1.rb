e0633e1cca1bd52d67b6851ecc9d462e72fbc277
Jamalludin <jamal.schneider@gmail.com>
2015-06-12 23:50:04 +0700

    membuat perulangan dengan menggunakan for

--- Jamal/loops/loops1.rb
+++ Jamal/loops/loops1.rb
@@ -9,3 +9,9 @@ while $i < $num  do
    puts("Ini perulangan ke. #$i" )
    $i +=1
 end
+
+#perulangan dengan for
+puts "Masukan banyak perulangan : "
+for i in 0..8
+   puts "Ini perulangan menggunakan for ke. #{i}"
+end
