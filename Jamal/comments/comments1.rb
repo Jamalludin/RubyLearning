9f3127d8704fe78ed3569190b11f00ed7ac6a6a1
Jamalludin <jamal.schneider@gmail.com>
2015-06-12 23:15:13 +0700

    membuat sebuah komentar di ruby

---
+++ Jamal/comments/comments1.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/ruby -w
+
+puts "Hello, Ruby!"
+
+=begin
+This is a multiline comment and con spwan as many lines as you
+like. But =begin and =end should come in the first line only. 
+=end
+puts "jamalludin"
+=begin
+ini adalah komentar dengan banyak baris yang di baut dengan bahasa ruby 
+yang dimualai dengan "=begin " dan harus di akhiri dengan "=end"
+=end
