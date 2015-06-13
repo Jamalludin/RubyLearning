c8e7ca1fa1cadc590af31338329f21cddc3c7b63
Jamalludin <jamal.schneider@gmail.com>
2015-06-13 16:29:45 +0700

    memcoba sub menu blocks

---
+++ Jamal/8-blocks/blocks1.rb
@@ -0,0 +1,15 @@
+def test
+   puts "sebelum angka 3 angka berapa ?"
+   yield 
+   puts "0 , 1 , dan angka ?"
+   yield
+   puts "bilangan genap adalah ?"
+   yield
+   puts "abis angka 1 angka berapa ?"
+   yield
+end
+test {puts "2 (dua)"}
+=begin
+	fungsi yield untuk memanggil
+	fungsi test {puts "2 (dua"}
+=end
