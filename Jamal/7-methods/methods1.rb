1385a7fa8728c8843c4a1a40cca265382654566b
Jamalludin <jamal.schneider@gmail.com>
2015-06-13 09:01:50 +0700

    mengetahui banyak parameters yang di buat

--- Jamal/7-methods/methods1.rb
+++ Jamal/7-methods/methods1.rb
@@ -7,7 +7,7 @@ def coba(jam="Ruby", al="java", lu="pascal")
 end
 coba "C", "C++" , "java SE" #membuat kondisi baru untuk di panggil
 coba #memanggil def yang sudah di buat
-
+puts"===================================="
 #menambah beberapa baris program
 def coba2 
    j = 100
@@ -19,3 +19,20 @@ return j , a , m , l , hasil
 end
 panggil = coba2
 puts panggil
+
+puts"===================================="
+=begin 
+untuk mengetahui banyak parameter yang di buat 
+dan apa saya parameter tersebut
+=end
+def contoh (*test)
+   puts "banyak parameters : #{test.length}"
+   for i in 0...test.length
+      puts "parameters #{test[i]}"
+   end
+end
+contoh "Zara", "6", "F"
+contoh "Mac", "36", "M", "MCA"
+contoh "jamal", "18", "L", "yukum" , "jaya"
+
+puts"===================================="
