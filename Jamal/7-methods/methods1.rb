0de2e0bf1ce896a2216063f098a6ea3e8facca80
Jamalludin <jamal.schneider@gmail.com>
2015-06-13 08:23:53 +0700

    menambahkan beberapa baris program dengan methods

--- Jamal/7-methods/methods1.rb
+++ Jamal/7-methods/methods1.rb
@@ -7,3 +7,15 @@ def coba(jam="Ruby", al="java", lu="pascal")
 end
 coba "C", "C++" , "java SE" #membuat kondisi baru untuk di panggil
 coba #memanggil def yang sudah di buat
+
+#menambah beberapa baris program
+def coba2 
+   j = 100
+   a = 200
+   m = 300
+   l = 400
+   hasil =j+a+m+l
+return j , a , m , l , hasil
+end
+panggil = coba2
+puts panggil
