711cc5436cbeab1a1b302fbf848138b8f8b8b525
Jamalludin <jamal.schneider@gmail.com>
2015-06-13 08:21:24 +0700

    membuat methods di ruby

---
+++ Jamal/7-methods/methods1.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/ruby
+
+def coba(jam="Ruby", al="java", lu="pascal")
+   puts "Bahasa Pemrograman #{jam}"
+   puts "Bahasa Pemrograman #{al}"
+   puts "Bahasa Pemrograman #{lu}"
+end
+coba "C", "C++" , "java SE" #membuat kondisi baru untuk di panggil
+coba #memanggil def yang sudah di buat
