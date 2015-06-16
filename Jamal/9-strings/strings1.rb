22a07c4411eabf70865c8ea47b699f0e5a0fcd7b
Jamalludin <jamal.schneider@gmail.com>
2015-06-16 11:20:32 +0700

    membaut contoh 1 string dalam ruby

---
+++ Jamal/9-strings/strings1.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/ruby
+
+#contoh string dalam ruby pertama 
+j, a, m = 7, 9, 6
+puts "nilai dari j adalah #{ j }."
+puts "hasil perkalian dari nilai j dan a adalah #{ j * a }."
+puts "hasil rata-ratanya adalah #{ (j + a + m)/3 }."
+puts "hasil pengurangan dari nila a dan m adalah #{a - m}."
+
+# contoh string kedua dalam ruby 
+
