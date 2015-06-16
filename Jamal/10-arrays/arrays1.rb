8e190dd5a3923870503931845b9ca647c391e833
Jamalludin <jamal.schneider@gmail.com>
2015-06-16 11:52:59 +0700

    contoh array ke 1 dalam ruby

---
+++ Jamal/10-arrays/arrays1.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/ruby
+
+#contoh array 1
+coba = Array.new(10)	#array yang bernilai 10
+puts "ukuran array #{coba.size}"  # This returns 10
+puts "panjang array #{coba.length}" # This also returns 10
+
+#contoh array 2
