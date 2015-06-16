44c83b5a0ada803b52fde59426b935b083f64e9c
Jamalludin <jamal.schneider@gmail.com>
2015-06-16 15:35:04 +0700

    mencoba sub menu hashes ke 2 dan 3

--- Jamal/11-hashes/hashes1.rb
+++ Jamal/11-hashes/hashes1.rb
@@ -5,3 +5,13 @@ bulan = Hash.new( "bulan januari" )
 puts "muncul #{bulan[100]}"
 puts "tampil #{bulan[76]}"
 
+#coba hashes 2
+H2 = Hash["j" => 96, "a" => 20]
+puts "nilai j = #{H2['j']}"
+puts "nilai a = #{H2['a']}"
+
+#coba hashes 3 menampilkan no urut 
+$, = ", "
+months = {"1" => "ahmad", "2" => "waqid" , "3" => "cantya"}
+keys = months.keys
+puts "#{keys}"
