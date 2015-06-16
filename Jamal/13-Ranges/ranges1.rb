9f56cb760c0641bd287ac5e481015c64542e350e
Jamalludin <jamal.schneider@gmail.com>
2015-06-16 16:21:19 +0700

    membuat ranges di ruby ke 2

--- Jamal/13-Ranges/ranges1.rb
+++ Jamal/13-Ranges/ranges1.rb
@@ -5,3 +5,20 @@ range1 = (1..7).to_a
 range2 = ('bas'..'bat').to_a
 puts "#{range1}"
 puts "#{range2}"
+
+#contoh 2
+digits = 1..7
+
+#puts digits.include?(5)
+ret = digits.min
+puts "nilai Min #{ret}"
+
+ret = digits.max
+puts "nilai Max #{ret}"
+
+ret = digits.reject {|i| i < 5 }
+puts "tampilkan nilai >= 5 :  #{ret}"
+
+digits.each do |digit|
+puts "In Loop #{digit}"
+end
